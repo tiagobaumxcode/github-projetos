bea65e645efa4e2945e31608b970fad024c58a7f
tiagobaumxcode <tiagobaumxcode@gmail.com>
2025-03-09 07:34:24 -0300

    Criando os modelos

---
+++ washpetbaum/Modelos/Animal.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class Animal{
+    public function __construct(){
+        $this->name = "";
+}
+public function getName(){
+    return $this->name;
+}
+}
+
+
---
+++ washpetbaum/Modelos/Pessoa.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Pessoa {
+     private string $nome;
+     private string $cpf;
+     
+     public function __construct(string $nome, string $cpf) {
+        $this->nome = $nome;    
+        $this->cpf = $cpf;
+
+     }
+
+
+
+
+
+}
